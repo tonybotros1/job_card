9220a6add990a06cdd76d9d7557bc44ff509dfb9
tonybotros1 <tonybotros6@gmail.com>
2023-09-27 17:23:33 +0300

    added search

--- lib/controllers/all_works_screen_controller.dart
+++ lib/controllers/all_works_screen_controller.dart
@@ -1,8 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
+import 'package:job_card/models/job_card_model.dart';
 
 class AllWorksController extends GetxController {
   final RxList<DocumentSnapshot> carCards = RxList<DocumentSnapshot>([]);
+  final RxList<DocumentSnapshot> filteredCarCards =
+      RxList<DocumentSnapshot>([]);
 
   @override
   void onInit() {
@@ -13,11 +16,37 @@ class AllWorksController extends GetxController {
 // this function is to get the works from firebase
   void getAllWorks() {
     FirebaseFirestore.instance
-        .collection('car_card').orderBy('timestamp', descending: true)
+        .collection('car_card')
+        .orderBy('timestamp', descending: true)
         .snapshots()
         .listen((event) {
       carCards.assignAll(event.docs);
     });
-    // print(carCards);
+  }
+
+  // Function to filter the list based on search criteria
+  void filterResults(String query) {
+    query = query.toLowerCase();
+
+    // Use where() to filter the list based on multiple fields
+    List<DocumentSnapshot> filteredResults = carCards.where((documentSnapshot) {
+      final data = documentSnapshot.data() as Map<String, dynamic>?;
+
+      final customerName = data?['customer_name'] ?? '';
+      final carBrand = data?['car_brand'] ?? '';
+      final carModel = data?['car_model'] ?? '';
+      final platNumber = data?['plate_number'] ?? '';
+      final date = data?['date'] ?? '';
+
+      // Check if any of the fields start with the query
+      return customerName.toString().toLowerCase().contains(query) ||
+          carBrand.toString().toLowerCase().contains(query) ||
+          carModel.toString().toLowerCase().contains(query) ||
+          platNumber.toString().toLowerCase().contains(query) ||
+          date.toString().toLowerCase().contains(query);
+    }).toList();
+
+    // Update the list with the filtered results
+    filteredCarCards.assignAll(filteredResults);
   }
 }
--- lib/screens/all_works_screen.dart
+++ lib/screens/all_works_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:job_card/const.dart';
@@ -19,6 +20,13 @@ class AllWorksScreen extends StatelessWidget {
           title: const Text('All Cards'),
           centerTitle: true,
           backgroundColor: mainColor,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  showSearch(context: context, delegate: DataSearch());
+                },
+                icon: const Icon(Icons.search))
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           shape: RoundedRectangleBorder(
@@ -33,7 +41,7 @@ class AllWorksScreen extends StatelessWidget {
             },
           ),
         ),
-        body: GetX(
+        body: GetX<AllWorksController>(
             init: AllWorksController(),
             builder: (controller) {
               if (controller.carCards.isEmpty) {
@@ -55,7 +63,7 @@ class AllWorksScreen extends StatelessWidget {
                         elevation: 20,
                         child: Padding(
                             padding: const EdgeInsets.all(8.0),
-                            child: GestureDetector(
+                            child: InkWell(
                                 onTap: () {
                                   Get.to(() => EditCardScreen(),
                                       arguments: JobCardModel(
@@ -128,6 +136,126 @@ class AllWorksScreen extends StatelessWidget {
                     });
               }
             }));
+  }
+}
+
+class DataSearch extends SearchDelegate {
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+          onPressed: () {
+            query = "";
+          },
+          icon: const Icon(Icons.close))
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+        onPressed: () {
+          close(context, null);
+        },
+        icon: const Icon(Icons.arrow_back));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return GetX<AllWorksController>(builder: (controller) {
+      final AllWorksController allWorksController =
+          Get.put(AllWorksController());
+
+      allWorksController.filterResults(query);
+
+      if (controller.filteredCarCards.isEmpty) {
+        return Center(
+            child: Text(
+          'No Cards Yet',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, color: mainColor, fontSize: 25),
+        ));
+      } else {
+        return ListView.builder(
+            itemCount: controller.filteredCarCards.length,
+            shrinkWrap: true,
+            itemBuilder: (context, i) {
+              var carCard = controller.filteredCarCards[i];
+              return Card(
+                elevation: 20,
+                child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: InkWell(
+                        onTap: () {
+                          Get.to(() => EditCardScreen(),
+                              arguments: JobCardModel(
+                                  carBrand: carCard['car_brand'],
+                                  carMileage: carCard['car_mileage'],
+                                  carModel: carCard['car_model'],
+                                  chassisNumber: carCard['chassis_number'],
+                                  color: carCard['color'],
+                                  customerName: carCard['customer_name'],
+                                  date: carCard['date'],
+                                  emailAddress: carCard['email_address'],
+                                  fuelAmount: carCard['fuel_amount'],
+                                  phoneNumber: carCard['phone_number'],
+                                  plateNumber: carCard['phone_number'],
+                                  docID: carCard.id),
+                              transition: Transition.leftToRight);
+                        },
+                        child: SizedBox(
+                          height: 100,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  FittedBox(
+                                      child: Text(
+                                    '${carCard['customer_name']}',
+                                    style: fontStyle,
+                                  )),
+                                  FittedBox(
+                                      child: Text(
+                                    '${carCard['date']}',
+                                    style: fontStyle2,
+                                  ))
+                                ],
+                              ),
+                              Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    '${carCard['car_brand']}',
+                                    style: fontStyle2,
+                                  ),
+                                  Text(
+                                    '${carCard['car_model']}',
+                                    style: fontStyle2,
+                                  ),
+                                  Text(
+                                    '${carCard['plate_number']}',
+                                    style: fontStyle2,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ))),
+              );
+            });
+      }
+    });
   }
 }
 
