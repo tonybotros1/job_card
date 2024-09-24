43e3dca64e16c55c65e27dd1322ebb8621bd2ae6
tonybotros1 <tonybotros1@bitbucket.org>
2024-09-25 00:14:40 +0300

    big fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -17,30 +15,50 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Platform.isAndroid || Platform.isIOS
-      ? await Firebase.initializeApp(
-          options: const FirebaseOptions(
-          apiKey: 'AIzaSyB2ql5CI0sSUQ0igdwh0G1Bmsu6TUtBpIw',
-          appId: '1:9681419746:android:278788a51a443d7647cdbb',
-          messagingSenderId: '9681419746',
-          storageBucket: 'job-card-62478.appspot.com',
-          projectId: 'job-card-62478',
-        ))
-      : kIsWeb
-          ? await Firebase.initializeApp(
-              options: const FirebaseOptions(
-                  apiKey: "AIzaSyDQE2wU5fSbsePR5XAzhxRhHv3eHppwv94",
-                  authDomain: "job-card-62478.firebaseapp.com",
-                  projectId: "job-card-62478",
-                  storageBucket: "job-card-62478.appspot.com",
-                  messagingSenderId: "9681419746",
-                  appId: "1:9681419746:web:4f6f116df48e511f47cdbb",
-                  measurementId: "G-LDQZ5NLESB"))
-          : await Firebase.initializeApp();
-  await FirebaseAppCheck.instance.activate(
-    webProvider: ReCaptchaV3Provider('recaptcha-v3-site-key'),
-    androidProvider: AndroidProvider.debug,
-  );
+  // Platform.isAndroid || Platform.isIOS
+  //     ? await Firebase.initializeApp(
+  //         options: const FirebaseOptions(
+  //         apiKey: 'AIzaSyB2ql5CI0sSUQ0igdwh0G1Bmsu6TUtBpIw',
+  //         appId: '1:9681419746:android:278788a51a443d7647cdbb',
+  //         messagingSenderId: '9681419746',
+  //         storageBucket: 'job-card-62478.appspot.com',
+  //         projectId: 'job-card-62478',
+  //       ))
+  //     : kIsWeb
+  //         ? await Firebase.initializeApp(
+  //             options: const FirebaseOptions(
+  //                 apiKey: "AIzaSyDQE2wU5fSbsePR5XAzhxRhHv3eHppwv94",
+  //                 authDomain: "job-card-62478.firebaseapp.com",
+  //                 projectId: "job-card-62478",
+  //                 storageBucket: "job-card-62478.appspot.com",
+  //                 messagingSenderId: "9681419746",
+  //                 appId: "1:9681419746:web:4f6f116df48e511f47cdbb",
+  //                 measurementId: "G-LDQZ5NLESB"))
+  //         : await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyDQE2wU5fSbsePR5XAzhxRhHv3eHppwv94",
+            authDomain: "job-card-62478.firebaseapp.com",
+            projectId: "job-card-62478",
+            storageBucket: "job-card-62478.appspot.com",
+            messagingSenderId: "9681419746",
+            appId: "1:9681419746:web:4f6f116df48e511f47cdbb",
+            measurementId: "G-LDQZ5NLESB"));
+  } else {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+      apiKey: 'AIzaSyB2ql5CI0sSUQ0igdwh0G1Bmsu6TUtBpIw',
+      appId: '1:9681419746:android:278788a51a443d7647cdbb',
+      messagingSenderId: '9681419746',
+      storageBucket: 'job-card-62478.appspot.com',
+      projectId: 'job-card-62478',
+    ));
+  }
+  // await FirebaseAppCheck.instance.activate(
+  //   webProvider: ReCaptchaV3Provider('recaptcha-v3-site-key'),
+  //   androidProvider: AndroidProvider.debug,
+  // );
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -8,6 +8,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   firebase_auth
   firebase_core
   firebase_storage
+  flutter_js
   share_plus
   url_launcher_windows
 )
